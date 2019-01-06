cafdcce6700595cbcf4e9c7a88d91dfc2b034176
weimin <weimin02@meituan.com>
2019-01-06 16:50:28 +0800

    clear

--- src/test/java/com/free/winter/version2/dao/ItemDAO.java
+++
@@ -1,9 +0,0 @@
-package com.free.winter.version2.dao;
-
-/**
- * @author weimin02
- * @date 2018/8/1
- * @project winter
- */
-public class ItemDAO {
-}
--- src/test/java/com/free/winter/version3/dao/AccountDAO.java
+++
@@ -1,9 +0,0 @@
-package com.free.winter.version3.dao;
-
-/**
- * @author weimin02
- * @date 2018/8/3
- * @project winter
- */
-public class AccountDAO {
-}
